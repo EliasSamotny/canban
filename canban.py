aaeaa1d1a794745e76c7652767e054865b838972
Elias <ELias@root.com>
2022-12-16 23:08:41 +0800

    gathering info from ugile

--- canban.py
+++ canban.py
@@ -1,13 +1,12 @@
 import json
 import fake_useragent
-import base64
 import datetime as dt
 import requests
 from requests.structures import CaseInsensitiveDict
 
 url = 'https://yougile.com/api-v2/'
 
-def get_compans(log, pas):
+def get_compans(log, pas): # get companies by log et pass
     url_l = 'auth/companies'
     headers = CaseInsensitiveDict()
     data = {
@@ -15,13 +14,11 @@ def get_compans(log, pas):
         'password': pas
     }
     headers['Content-Type'] = "application/json"
-    user = fake_useragent.UserAgent().random
-    headers['User-Agent'] = user
+    headers['User-Agent'] = fake_useragent.UserAgent().random
     response = requests.post((url + url_l), headers = headers,json = data)
-    resp_dict = response.json()
     
-    return resp_dict
-def get_keys(log, pas, compId):
+    return response
+def get_keys(log, pas, compId): # get key pour company
     url_l = 'auth/keys/get'
     headers = CaseInsensitiveDict()
     data = {
@@ -30,41 +27,34 @@ def get_keys(log, pas, compId):
          "companyId": compId
     }
     headers['Content-Type'] = "application/json"
-    user = fake_useragent.UserAgent().random
-    headers['User-Agent'] = user
+    headers['User-Agent'] = fake_useragent.UserAgent().random
     response = requests.post((url + url_l), headers = headers,json = data)
-    resp_dict = response.json()
     
-    return resp_dict
+    return response
 
-def create_key(log, pas, compId):
+def create_key(log, pas, compId): # create a key pour company
     url_l = 'auth/keys'
     headers = CaseInsensitiveDict()
     data = {
         'login': log,
         'password': pas,
-         "companyId": compId
+        "companyId": compId
     }
     headers['Content-Type'] = "application/json"
-    user = fake_useragent.UserAgent().random
-    headers['User-Agent'] = user
+    headers['User-Agent'] = fake_useragent.UserAgent().random
     response = requests.post((url + url_l), headers = headers,json = data)
-    resp_dict = response.json()
-    
-    return resp_dict
+    return response
 
-def del_key (key):
+def del_key (key): # delete a key of company
     url_l = 'auth/keys/' + key
     headers = CaseInsensitiveDict()
     headers['Content-Type'] = "application/json"
-    user = fake_useragent.UserAgent().random
-    headers['User-Agent'] = user
+    headers['User-Agent'] = fake_useragent.UserAgent().random
     response = requests.post((url + url_l), headers = headers)
-    resp_dict = response.json()
     
-    return resp_dict
+    return response
 
-def get_workers(key):
+def get_workers(key): # get list of workers of comp
     url_l = 'users'
     headers = {
         "Content-Type": "application/json",
@@ -72,17 +62,81 @@ def get_workers(key):
         'User-Agent' : fake_useragent.UserAgent().random
     }
     response = requests.post((url + url_l), headers = headers)
-    resp_dict = response.json()
+    return response
     
-    return resp_dict
-    
-def get_proj(key):
+def get_proj(key): # get projects by key
     url_l = 'projects'
     headers = CaseInsensitiveDict()
     headers["Content-Type"] = "application/json"
     headers["Authorization"] = "Bearer " + key
-    headers["includeDeleted"] = "True"
+    headers["includeDeleted"] = "False"
+    headers['User-Agent'] = fake_useragent.UserAgent().random
     response = requests.get((url + url_l), headers = headers)
-    resp_dict = response.json()
     
-    return resp_dict
+    return response
+
+def get_board(key, projId):
+    
+    url_l = "boards"
+    headers = CaseInsensitiveDict()
+    querystring = {"projectId":projId}
+    headers["Content-Type"] = "application/json"
+    headers["Authorization"] = "Bearer " + key
+    headers["includeDeleted"] = "False"
+    headers['User-Agent'] = fake_useragent.UserAgent().random
+    response = requests.get((url + url_l), headers = headers, params=querystring)
+    
+    return response
+
+def get_cols(key,board_id):
+    
+    url_l = "columns"
+    headers = CaseInsensitiveDict()
+    querystring = {"boardId": board_id}
+    headers["Content-Type"] = "application/json"
+    headers["Authorization"] = "Bearer " + key
+    headers["includeDeleted"] = "False"
+    headers['User-Agent'] = fake_useragent.UserAgent().random
+    response = requests.get((url + url_l), headers = headers, params=querystring)
+    
+    return response
+
+def get_tasks(key, col_id):
+    
+    url_l = "tasks"
+    headers = CaseInsensitiveDict()
+    querystring = {"columnId": col_id}
+    headers["Content-Type"] = "application/json"
+    headers["Authorization"] = "Bearer " + key
+    headers["includeDeleted"] = "False"
+    headers['User-Agent'] = fake_useragent.UserAgent().random
+    response = requests.get((url + url_l), headers = headers, params=querystring)
+    
+    return response
+
+def get_users_in_task(key, us_ids):
+    if type(us_ids) is str:
+        url_l = "users/" + us_ids
+        headers = CaseInsensitiveDict()
+        headers["Content-Type"] = "application/json"
+        headers["Authorization"] = "Bearer " + key
+        headers['User-Agent'] = fake_useragent.UserAgent().random
+        response = requests.get((url + url_l), headers = headers)
+        if response.status_code == 200:
+            response = response.json()
+            return response['realName']
+    else:
+        infa = []
+        for i in range(len(us_ids)):
+            url_l = "users/" + us_ids[i]
+            headers = CaseInsensitiveDict()
+            headers["Content-Type"] = "application/json"
+            headers["Authorization"] = "Bearer " + key
+            headers['User-Agent'] = fake_useragent.UserAgent().random
+            response = requests.get((url + url_l), headers = headers)
+            if response.status_code == 200:
+                response = response.json()
+                print(response)
+                infa.append(response['realName'])
+        return infa
+    
--- main.py
+++ main.py
@@ -1,9 +1,62 @@
-from canban import get_compans,get_keys
+from canban import create_key, get_board, get_cols, get_compans,get_keys,get_proj, get_tasks, get_users_in_task
 
 log = 'ilya.madaev2002e@gmail.com'
 pas = 'jEsuisFragile_1'
 
-comp = get_compans(log,pas)['content'][0]['id']
+comp = get_compans(log,pas).json()['content'][0]['id']
 
-key = get_keys(log,pas,comp)[0]['key']
-print (comp + ' et ' + key)
+key_req = get_keys(log,pas,comp)
+if key_req.status_code == 200:
+    key = key_req.json()[0]['key']
+else:
+    key = create_key(log, pas, comp).json()["key"]
+
+print (key)
+req_proj = get_proj(key)
+
+if req_proj.status_code == 200:
+    req_proj = req_proj.json()
+    proj = []
+    proj.append(req_proj['content'][0]['id']) 
+    proj.append(req_proj['content'][0]['title'])    
+    
+    req_board = get_board(key, proj[0])
+    req_board_json = req_board.json()
+    if req_board.status_code == 200 and int(req_board_json['paging']["count"]) > 0:
+        proj.append([req_board_json['content'][0]['id'],[]]) # the board
+
+        req_columns = get_cols(key,proj[2][0])
+        req_columns_json = req_columns.json()
+        if req_columns.status_code == 200 and int(req_columns_json['paging']["count"]) > 0:
+            for i in range(int(req_columns_json['paging']["count"])):
+                proj[2][1].append([req_columns_json['content'][i]['id'],
+                            req_columns_json['content'][i]['title'],
+                            []]) # the board
+                
+                req_tasks = get_tasks(key, proj[2][1][i][0])
+                req_tasks_json = req_tasks.json()
+                if req_tasks.status_code == 200:
+                    for j in range(int(req_tasks_json['paging']["count"])):
+                        cont = [
+                            req_tasks_json['content'][j]['id'],
+                            req_tasks_json['content'][j]['title'],                     
+                        ]
+                        if 'description' in req_tasks_json['content'][j]:
+                            cont.append(req_tasks_json['content'][j]['description'])
+                        else: cont.append('')
+                        if 'completed' in req_tasks_json['content'][j]:    
+                            cont.append(req_tasks_json['content'][j]['completed'])
+                        else: cont.append('')
+                        print (req_tasks_json['content'][j])
+                        cont.append(get_users_in_task(key, req_tasks_json['content'][j]['assigned']))
+                        
+                        proj[2][1][i][2].append(cont)
+                else: print ("Task \""+ proj[2][j][1] + '\" with id \"'+ proj[2][j][0] + "\" n\'existe pas!")
+    
+    elif int(req_board_json['paging']["count"]) > 0:
+        print ('No boards in project \"' + proj[1] + "\"")  
+    else: print ("Something wrong about boards")
+    
+    print (proj)    
+else: print ('No information about project.')
+
